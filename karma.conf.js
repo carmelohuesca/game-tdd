42bb6601f28de314faddf877f2638e7cb5e8b22e
Carmelo Vicente Huesca Calatayud <carmelovicente.huesca@aqualogybs.net>
2016-12-24 00:17:32 +0100

    incluir procesadores de cobertura y jshint

--- karma.conf.js
+++ karma.conf.js
@@ -26,14 +26,21 @@ module.exports = function(config) {
 
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-        preprocessors: {},
+        preprocessors: {
+            'src/**/*.js': ['jshint', 'coverage']
+        },
 
 
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress', 'kjhtml'],
+        reporters: ['progress', 'coverage', 'kjhtml'],
 
+        // optionally, configure the reporter
+        coverageReporter: {
+            type: 'html',
+            dir: 'coverage/'
+        },
 
         // web server port
         port: 9876,
